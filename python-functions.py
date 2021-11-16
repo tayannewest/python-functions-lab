f9dba5ee74f50ab2ff5d6c45234791d814d45772
Tay West <tayanne.west@gmail.com>
2021-11-16 14:39:24 -0500

    solve exercise 2

---
+++ python-functions.py
@@ -0,0 +1,25 @@
+# problem 1
+
+def sum_to(n):
+  sum = 0
+  for num in range(1, n + 1):
+    sum += num
+  return sum
+print(sum_to(6))
+
+
+# problem 2
+
+def largest(list):
+  list.sort()
+  return list[-1]
+
+print(largest([3, 5, 2, 1, 3]))
+
+# problem 3
+
+
+
+
+# problem 4
+
